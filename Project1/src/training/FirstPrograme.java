22303df9662a21528bfae0859bbd4f81c5e8fb1f
sabhajeet19 <sabhajeetverma19@gmail.com>
2022-12-25 21:11:11 +0530

    new changes

--- Project1/src/training/FirstPrograme.java
+++ Project1/src/training/FirstPrograme.java
@@ -5,6 +5,9 @@ public class FirstPrograme {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi all how are you.I am good");
+		int a = 25;
+		int b = 35;
+		System.out.println(a*b);
 		
 
 	}
