5989f2e0b020b1727952eba4ee1f1a2816a2078c
sabhajeet19 <sabhajeetverma19@gmail.com>
2022-12-25 16:36:32 +0530

    first commit

---
+++ Project1/src/training/FirstPrograme.java
@@ -0,0 +1,11 @@
+package training;
+
+public class FirstPrograme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi all how are you.");
+
+	}
+
+}
