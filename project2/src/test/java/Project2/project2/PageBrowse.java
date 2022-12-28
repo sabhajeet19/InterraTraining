8e2089ae6971a250f7d6aa240e6a3c11d9c5782a
sabhajeet19 <sabhajeetverma19@gmail.com>
2022-12-28 16:22:30 +0530

    project2 updated

--- project2/src/test/java/Project2/project2/PageBrowse.java
+++ project2/src/test/java/Project2/project2/PageBrowse.java
@@ -1,6 +1,7 @@
 package Project2.project2;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -8,8 +9,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import com.google.auto.common.Visibility;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -18,18 +22,26 @@ public class PageBrowse {
 	
 	//static WebDriver driver;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		String url ="https://www.amazon.in/";
 		driver.get(url);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		System.out.println("Hi how are you");
 		WebElement searchBar = driver.findElement(By.id("twotabsearchtextbox"));
-		searchBar.sendKeys("Allensolly");
+		searchBar.sendKeys("Allen Solly");
 		searchBar.sendKeys(Keys.ENTER);
+		WebElement discount = driver.findElement(By.linkText("60% Off or more"));
+		discount.click();
+		Thread.sleep(5000);
+		List<WebElement> ls = driver.findElements(By.xpath("//img[@class='s-image']"));
+		System.out.println("Number of products that have more than 60% discount =  "+ls.size());
+		
+				
 
 	}
 
